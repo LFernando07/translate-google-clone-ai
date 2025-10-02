5b6aa8de5ba6b5cd2d4b250069edcb8eeabbf494
Fernando <luis.ferburpe@hotmail.com>
2025-10-01 23:08:25 -0600

    remove config folder

--- translate-backend/config/env.js
+++
@@ -1,4 +0,0 @@
-export const isProduction = process.env.NODE_ENV === "production";
-export const PORT = process.env.PORT || 3001;
-export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-export const FRONTEND_URL = process.env.FRONTEND_URL;
